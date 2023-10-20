fc97c1433559cc919ac6c2fb5360b714bf8cfceb
Rakesh Arunachalam <rakesh@thoughtbot.com>
2023-10-20 16:04:05 +0100

    Add react navigation
    - Added react navigation to the project
    - Adde stack navigator to the project
    - Added a new screen and navigate to it.

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text, Image, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { styles } from "./styles";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.helloWorld}>Hello World!</Text>
+      <Text>A second text</Text>
+      <Image
+        source={{
+          uri: "https://png.pngtree.com/png-clipart/20230511/ourmid/pngtree-isolated-cat-on-white-background-png-image_7094927.png",
+        }}
+        style={styles.catImage}
+      />
+      <Button
+        title="To Dog"
+        onPress={() => {
+          navigation.navigate("Dog");
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DogScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.helloWorld}>Woof woof</Text>
+      <Text>I'm a dog</Text>
+      <Image
+        source={{
+          uri: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+        }}
+        style={styles.catImage}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Cat Home",
+          }}
+        />
+        <Stack.Screen
+          name="Dog"
+          component={DogScreen}
+          options={{
+            title: "Dog Home",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  helloWorld: {
+    color: "red",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  catImage: { width: 200, height: 200 },
+});
